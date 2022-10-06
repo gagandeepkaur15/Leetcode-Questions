185ed858c164a3f1a16a8b1c2a8ec882ff171ea1
gagandeepkaur15 <104858357+gagandeepkaur15@users.noreply.github.com>
2022-10-06 19:06:37 +0530

    Added length-of-last-word in python
    Added solution to length-of-last-word in python

--- Easy/length_of_last_word.py
+++ Easy/length_of_last_word.py
@@ -1,3 +1,6 @@
+#Given a string s consisting of words and spaces, return the length of the last word in the string.
+#A word is a maximal substring consisting of non-space characters only. 
+
 def lengthOfLastWord(str):
     count = 0
     x = str.split()
